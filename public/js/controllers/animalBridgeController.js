99d40ef0fc730ca63db96fe098091927871d96a4
Nelson <nelson.liang@sjsu.edu>
2016-11-14 19:29:55 -0800

    fixed layout problem

--- public/js/controllers/animalBridgeController.js
+++ public/js/controllers/animalBridgeController.js
@@ -1,25 +1,25 @@
 var app = angular.module('AnimalBridgeApp', ['ngRoute']);
 
 jQuery(document).ready(function() {
-    $('.launch-modal').on('click', function(e) {
+    $('.launch-modal').on('click', function(e){
         e.preventDefault();
-        $('#' + $(this).data('modal-id')).modal();
+        $( '#' + $(this).data('modal-id') ).modal();
     });
 });
 
 
-app.config(["$routeProvider", "$locationProvider", function($routeProvider, $locationProvider) {
+app.config(["$routeProvider","$locationProvider",function($routeProvider, $locationProvider) {
     $routeProvider
     //route for the homepage
         .when('/', {
-            templateUrl: 'home.html',
-            controller: 'animalBridgeController'
-        })
-        // //route for the homepage
-        //     .when('/#/', {
-        //     templateUrl: 'home.html',
-        //     controller: 'animalBridgeController'
-        // })
+        templateUrl: 'home.html',
+        controller: 'animalBridgeController'
+    })
+    // //route for the homepage
+    //     .when('/#/', {
+    //     templateUrl: 'home.html',
+    //     controller: 'animalBridgeController'
+    // })
 
 
     //route for about page
@@ -36,7 +36,7 @@ app.config(["$routeProvider", "$locationProvider", function($routeProvider, $loc
 
     .when('/emergency', {
         templateUrl: 'emergency.html',
-        controller: 'emergencyController'
+        controller: 'contactController'
     });
 
     $locationProvider.html5Mode(true);
@@ -55,57 +55,4 @@ app.controller('aboutController', function($scope, $http) {
 app.controller('contactController', function($scope, $http) {
     $scope.name = "Contact Us";
     this.name = "Email: admin@animalbridge.org";
-});
-
-app.controller('emergencyController', function($scope, $http) {
-    $scope.name = "Emergency";
-    this.name = "Email: admin@animalbridge.org";
-
-    var map;
-    var infowindow;
-
-    initialize();
-    // google.maps.event.addDomListener(window, 'load', initialize);
-
-    function initialize() {
-        var pyrmont = new google.maps.LatLng(37.773972, -122.431297);
-
-        map = new google.maps.Map(document.getElementById('gmap_canvas'), {
-            center: pyrmont,
-            zoom: 8
-        });
-
-        console.log("trying to init gmap");
-
-        var request = {
-            location: pyrmont,
-            radius: 1000,
-            bounds: map.getBounds(),
-            keyword: ['veterinarians']
-        };
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch(request, callback);
-    };
-
-    function callback(results, status) {
-        if (status == google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
-        }
-    };
-
-    function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-        });
-
-        google.maps.event.addListener(marker, 'click', function() {
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
-        });
-    };
 });
--- public/js/controllers/emergency.js
+++ public/js/controllers/emergency.js
@@ -1,47 +1,44 @@
 var map;
 var infowindow;
-console.log("trying to init gmap before init");
 
 function initialize() {
-    var pyrmont = new google.maps.LatLng(37.773972, -122.431297);
+  var pyrmont = new google.maps.LatLng(37.773972, -122.431297);
 
-    map = new google.maps.Map(document.getElementById('gmap_canvas'), {
-        center: pyrmont,
-        zoom: 8
-    });
+  map = new google.maps.Map(document.getElementById('gmap_canvas'), {
+    center: pyrmont,
+    zoom: 8
+  });
 
-    console.log("trying to init gmap");
-
-    var request = {
-        location: pyrmont,
-        radius: 1000,
-        bounds: map.getBounds(),
-        keyword: ['veterinarians']
-    };
-    infowindow = new google.maps.InfoWindow();
-    var service = new google.maps.places.PlacesService(map);
-    service.nearbySearch(request, callback);
+  var request = {
+    location: pyrmont,
+    radius: 1000,
+    bounds: map.getBounds(),
+    keyword: ['veterinarians'] 
+  };
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch(request, callback);
 }
 
 function callback(results, status) {
-    if (status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-            createMarker(results[i]);
-        }
+  if (status == google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
     }
+  }
 }
 
 function createMarker(place) {
-    var placeLoc = place.geometry.location;
-    var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-    });
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
 
-    google.maps.event.addListener(marker, 'click', function() {
-        infowindow.setContent(place.name);
-        infowindow.open(map, this);
-    });
+  google.maps.event.addListener(marker, 'click', function() {
+    infowindow.setContent(place.name);
+    infowindow.open(map, this);
+  });
 }
 
 google.maps.event.addDomListener(window, 'load', initialize);

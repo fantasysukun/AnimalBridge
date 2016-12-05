41c06f14f81f624910712723a21242daf4308f80
fantasysukun <188454747@qq.com>
2016-12-04 17:01:55 -0800

    InsertPost method updated

--- java/edu/sjsu/db/animalbridge_posting.java
+++ java/edu/sjsu/db/animalbridge_posting.java
@@ -3,6 +3,8 @@ package edu.sjsu.db;
 import java.awt.image.BufferedImage;
 import java.util.Date;
 
+import com.google.gson.annotations.SerializedName;
+
 /**
  * COPYRIGHT 2016 TeamMinion. All Rights Reserved. 
  * Animal Bridge
@@ -13,21 +15,35 @@ import java.util.Date;
 
 public class animalbridge_posting {
 
+	@SerializedName("Posting_ID")
 	private int Posting_ID;
+	@SerializedName("Posting_Categories")
 	private String Posting_Categories;
+	@SerializedName("Posting_Priority")
 	private String Posting_Priority;
+	@SerializedName("Posting_Title")
 	private String Posting_Title;
+	@SerializedName("Posting_Address")
 	private String Posting_Address;
+	@SerializedName("Posting_Description")
 	private String Posting_Description;
+	@SerializedName("Posting_Date")
 	private String Posting_Date;
+	@SerializedName("Posting_StartingTime")
 	private String Posting_StartingTime;
+	@SerializedName("Posting_EndingTIme")
 	private String Posting_EndingTIme;
+	@SerializedName("Posting_Image")
 	private BufferedImage Posting_Image;
+	@SerializedName("Posting_Price")
 	private String Posting_Price;
+	@SerializedName("Posting_ContactEmail")
 	private String Posting_ContactEmail;
+	@SerializedName("Posting_OwnerID")
 	private int Posting_OwnerID;
+	@SerializedName("Posting_OwnerName")
 	private String Posting_OwnerName;
-
+	
 	public animalbridge_posting (
 
 		int Posting_ID,
